e371281e8777bfcab9c021bfb860858117c399e6
Lucas Gratien <lucas.gratien@le-campus-numerique.fr>
2024-11-19 11:27:00 +0100

    try catch controller supp

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -18,49 +18,27 @@ export class UsersController {
 
   @Get()
   async findAll(): Promise<Users[]> {
-    try {
-      const users = await this.usersService.findAll();
-      if (!users || users.length === 0) {
-        throw new NotFoundException('No user found');
-      }
-      return users;
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        console.error('Error :', error.message);
-        throw error;
-      } else {
-        console.error('Unexpected error :', error.message || error);
-        throw new Error('Unable to retrieve users at the moment.');
-      }
+    const users = await this.usersService.findAll();
+    if (!users || users.length === 0) {
+      throw new NotFoundException('No user found');
     }
+    return users;
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Users> {
-    try {
-      const user = await this.usersService.findOne(id);
-      if (!user) {
-        throw new NotFoundException(`User with ID ${id} not found`);
-      }
-      return user;
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        console.error('Error :', error.message);
-        throw error;
-      } else {
-        console.error('Unexpected error :', error.message || error);
-        throw new Error(
-          `Unable to retrieve users with ID ${id} at the moment.`,
-        );
-      }
+    const user = await this.usersService.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
     }
+    return user;
   }
+
   @Post()
   async create(@Body() usersDto: UsersDto): Promise<Users> {
-    try {
-      const createdUser = new this.usersService.userModel(usersDto);
-      return createdUser.save();
-    }}
+    const createdUser = new this.usersService.userModel(usersDto);
+    return createdUser.save();
+  }
 
   @Put(':id')
   async update(
